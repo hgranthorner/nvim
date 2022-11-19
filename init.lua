171cc153ac708e77d36e7f050164342f993085b2
Grant Horner <h.grant.horner@gmail.com>
2022-11-19 16:58:47 -0500

    remove vlime

--- init.lua
+++ init.lua
@@ -42,7 +42,6 @@ require('packer').startup(function(use)
   use "tpope/vim-surround"
   use "mbbill/undotree"
   use "jose-elias-alvarez/null-ls.nvim"
-  use "vlime/vlime"
 end)
 vim.cmd("colorscheme nordfox")
 
